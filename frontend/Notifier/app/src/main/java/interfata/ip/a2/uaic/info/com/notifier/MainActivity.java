3fd7760784a500bc2b9a002721b1f805f82d1d73
Cosmin Ionut SCHIFIRNET <cschifirnet@ias.bitdefender.biz>
2017-05-01 22:53:09 +0300

    Updated gitignore and added the Notifier android app

---
+++ frontend/Notifier/app/src/main/java/interfata/ip/a2/uaic/info/com/notifier/MainActivity.java
@@ -0,0 +1,31 @@
+package interfata.ip.a2.uaic.info.com.notifier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // Example of a call to a native method
+    TextView tv = (TextView) findViewById(R.id.sample_text);
+    // tv.setText(stringFromJNI());
+    tv.setText("Salut");
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+/*    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+*/
+}
