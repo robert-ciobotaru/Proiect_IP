0bdcbce59475bef390974d95709fa1e795a9ff65
DamocValentin <valentin.damoc@yahoo.com>
2017-05-03 20:32:00 +0300

    Added getTriggeredNotifications

--- middle/MiddleEndModule/src/main/java/com/usermanagement/controllers/UsersRestController.java
+++ middle/MiddleEndModule/src/main/java/com/usermanagement/controllers/UsersRestController.java
@@ -1,6 +1,8 @@
 package com.usermanagement.controllers;
 
+import java.util.List;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
@@ -9,17 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import com.usermanagement.DTO.AddNotificationDto;
+import com.usermanagement.DTO.GetTriggeredNotificationsDto;
 import com.usermanagement.DTO.NotificationCreateDto;
 import com.usermanagement.DTO.NotificationDto;
+import com.usermanagement.DTO.NotificationsListDto;
 import com.usermanagement.DTO.UserDto;
 
 //@RefreshScope
 @RestController
 @RequestMapping("v1/users")
-class UsersRestController {
+public class UsersRestController {
 
     @RequestMapping(value = "/{userId}/notifications", method = RequestMethod.GET)
     public ResponseEntity<UserDto> getNotifications(@PathVariable("userId") Long userId) {
@@ -47,5 +52,34 @@ class UsersRestController {
     	 notification.setPeriod(notificationCreate.getPeriod());
     	 notification.setText(notificationCreate.getText());
     	return new ResponseEntity<>(notification, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/{userId}/notifications/triggered-notifications", method = RequestMethod.GET)
+    public ResponseEntity<NotificationsListDto> getTriggeredNotifications(@PathVariable("userId") Long userId){
+    	GetTriggeredNotificationsDto getTriggeredNotification = new GetTriggeredNotificationsDto();
+    	String url = new String("https://www.triburile.ro/");
+    	RestTemplate rest = new RestTemplate();
+    	getTriggeredNotification.setId(userId);
+    	
+    	try{
+       	 ResponseEntity<NotificationDto> response = rest.postForEntity(url,getTriggeredNotification,NotificationDto.class);
+       	 }
+       	 catch (Exception e) {
+       		 System.out.println(e);
+       	 }
+    	
+    	NotificationsListDto notificationsList = new NotificationsListDto();
+    	NotificationCreateDto notification1 = new NotificationCreateDto();
+    	NotificationCreateDto notification2 = new NotificationCreateDto();
+    	notification1.setText("Trezirea de dimineata");
+    	notification2.setText("Alarma de la 9:15");
+    	notification1.setPeriod("none");
+    	notification2.setPeriod("daily");
+    	notification1.setDay_timestamp(2315);
+    	notification2.setDay_timestamp(2315);
+    	notificationsList.notifications.add(notification1);
+    	notificationsList.notifications.add(notification2);
+    	
+        return new ResponseEntity<>(notificationsList, HttpStatus.OK);
     }
 }
