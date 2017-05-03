f6b6f7a1fc3942de076f86456d98f3c648aee47e
DamocValentin <valentin.damoc@yahoo.com>
2017-05-03 20:20:49 +0300

    Added getTriggeredNotifications

---
+++ middle/MiddleEndModule/src/main/java/com/usermanagement/DTO/NotificationsListDto.java
@@ -0,0 +1,8 @@
+package com.usermanagement.DTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationsListDto {
+	public List<NotificationCreateDto> notifications = new ArrayList<NotificationCreateDto>();
+}
