7abfdbbdd45d847ccd9333f5d6c0e58f2d35cc7e
Edu Dorin <eduu.dorin@gmail.com>
2017-05-04 12:01:23 +0300

    notification view

--- frontend/Notifier/app/.externalNativeBuild/cmake/debug/x86_64/cmake_install.cmake
+++ frontend/Notifier/app/.externalNativeBuild/cmake/debug/x86_64/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/AnII-SemestruII/IP/Proiect_IP/frontend/Notifier/app
+# Install script for directory: /Volumes/work/FII/Semestrul 4/IP/Proiect_IP/frontend/Notifier/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/AnII-SemestruII/IP/Proiect_IP/frontend/Notifier/app/.externalNativeBuild/cmake/debug/x86_64/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Volumes/work/FII/Semestrul 4/IP/Proiect_IP/frontend/Notifier/app/.externalNativeBuild/cmake/debug/x86_64/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ frontend/Notifier/app/src/main/java/interfata/ip/a2/uaic/info/com/notifier/NotificationSend.java
@@ -0,0 +1,59 @@
+package com.example.pethoalpar.androidnotificationexample;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Random;
+
+public class NotificationSend extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+
+    public EditText getEditText() {
+        return editText;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_send);
+
+        editText = (EditText) this.findViewById(R.id.editText);
+        button = (Button) this.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getBaseContext());
+                mBuilder.setSmallIcon(R.drawable.icon);
+                mBuilder.setContentText(text);
+                mBuilder.setContentTitle("Example");
+
+                Intent resultIntent = new Intent(getBaseContext(), NotificationView.class);
+                resultIntent.putExtra("text", text);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(getBaseContext());
+                stackBuilder.addParentStack(NotificationView.class);
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder.setContentIntent(resultPendingIntent);
+                mBuilder.setAutoCancel(true);
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                Random rand = new Random();
+                notificationManager.notify(rand.nextInt(130000),mBuilder.build());
+            }
+        });
+
+
+    }
+}
