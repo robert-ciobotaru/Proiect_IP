1d0e25181b067bce17e69f723c8878e8099b4599
Cosmin Ionut SCHIFIRNET <cschifirnet@ias.bitdefender.biz>
2017-05-04 21:01:08 +0300

    New base app

---
+++ frontend/Notifier/app/src/main/java/interfata/ip/notifier/Login.java
@@ -0,0 +1,29 @@
+package interfata.ip.notifier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button fab = (Button) findViewById(R.id.button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent categories = new Intent(getApplicationContext(),Categories.class);
+                startActivity(categories);
+            }
+        });
+    }
+}
---
+++ frontend/Notifier/app/src/main/java/interfata/ip/notifier/MainActivity.java
@@ -0,0 +1,35 @@
+package interfata.ip.notifier;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText("Salut");
+
+        Button button = (Button) findViewById(R.id.button);
+        Button button2= (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(getApplicationContext(), Login.class);
+                startActivity(login);
+            }
+        });
+    }
+
+
+}
