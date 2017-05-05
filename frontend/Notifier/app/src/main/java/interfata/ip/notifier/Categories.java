52229603e51b715436431a695eabf911059e7c2d
Silviu Munteanu <Silviu Munteanu>
2017-05-05 21:07:23 +0300

    fixed crash

--- frontend/Notifier/app/src/main/java/interfata/ip/notifier/Categories.java
+++ frontend/Notifier/app/src/main/java/interfata/ip/notifier/Categories.java
@@ -1,14 +1,13 @@
 package interfata.ip.notifier;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.os.Bundle;
 
 public class Categories extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(interfata.ip.notifier.R.layout.activity_categories);
+        setContentView(R.layout.activity_categories);
     }
-
 }
