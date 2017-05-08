3ba2dde567f371e3ab73c2e2d5a84d174c28099d
Raluca <pojar_raluca@yahoo.com>
2017-05-08 23:17:27 +0300

    add class settings notifier

---
+++ frontend/Notifier/app/src/main/java/interfata/ip/notifier/Settings/Settings.java
@@ -0,0 +1,11 @@
+package interfata.ip.notifier.Settings;
+
+/**
+ * Created by Pojar on 5/8/2017.
+ */
+
+public class Settings {
+    final static String URL= "http://104.198.253.69";
+    final static int Port=8080;
+    final static String Version= "v1";
+}
